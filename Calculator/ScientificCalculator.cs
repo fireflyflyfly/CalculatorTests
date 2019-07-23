45d07ed6cd20862aa199683a57ef066ce83ca5a1
Yegor  Abramenkov <yegorabramenkov@Yegors-MacBook-Pro.local>
2019-07-23 11:07:43 +0300

    test

--- Calculator/ScientificCalculator.cs
+++ Calculator/ScientificCalculator.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using System.Linq;
 namespace Calculator
 {
-    public class ScientificCalculator
+    public class ScientificCalculator : RegularCalculator
     {
-        public ScientificCalculator()
+        public double MathPowFunc(double basis, double exponent)
         {
+            return Math.Pow(basis, exponent);
+        }
+
+        public double GetPercent(double a, double b)
+        {
+            return Math.Round(a / b * 100);
+        }
+
+        public double GetArraySum(double[] doubleArray)
+        {
+            double total = 0;
+            foreach (double number in doubleArray)
+            {
+                total += number;
+            }
+            return total;
+        }
+
+        public double GetMaxArrayValue(double[] doubleArray)
+        {
+            double max = doubleArray.Max();
+            return max;
+        }
+
+        public double GetMinArrayValue(double[] doubleArray)
+        {
+            double min = doubleArray.Min();
+            return min;
         }
     }
 }
--- CalculatorTests/ScientificCalculatorTests.cs
+++ CalculatorTests/ScientificCalculatorTests.cs
@@ -1,10 +1,48 @@
 ﻿using System;
-namespace CalculatorTests
+using NUnit.Framework;
+using Calculator;
+namespace Tests
 {
+    [TestFixture]
     public class ScientificCalculatorTests
     {
-        public ScientificCalculatorTests()
+
+        [Test(Description = "MathPOW method verification"), Order(1)]
+        public void VerifyPow()
         {
+            ScientificCalculator calc = new ScientificCalculator();
+            Assert.AreEqual(4, calc.MathPowFunc(2, 2));
+        }
+
+        [Test(Description = "GetPerscent method verification"), Order(2)]
+        public void VerifyGetPercent()
+        {
+            ScientificCalculator calc = new ScientificCalculator();
+            Assert.AreEqual(5, calc.GetPercent(5, 100));
+        }
+
+        [Test(Description = ""), Order(3)]
+        public void VerifyGetArraySum()
+        {
+            double[] doubleArray = { 1, 2, 3 };
+            ScientificCalculator calc = new ScientificCalculator();
+            Assert.AreEqual(6, calc.GetArraySum(doubleArray));
+        }
+
+        [Test(Description = "GetMaxArrayValue method verification"), Order(4)]
+        public void VerifyGetMaxArrayValue()
+        {
+            double[] doubleArray = { 2, 2, 3, 5, 18, 0, 33, 1.6, 33.3 };
+            ScientificCalculator calc = new ScientificCalculator();
+            Assert.AreEqual(33.3, calc.GetMaxArrayValue(doubleArray));
+        }
+
+        [Test(Description = "GetMaxArrayValue method verification"), Order(5)]
+        public void VerifyGetMinArrayValue()
+        {
+            double[] doubleArray = { 2, 2, 3, 5, 18, 0, 33, 1.6, 33.3 };
+            ScientificCalculator calc = new ScientificCalculator();
+            Assert.AreEqual(33.3, calc.GetMinArrayValue(doubleArray));
         }
     }
 }
