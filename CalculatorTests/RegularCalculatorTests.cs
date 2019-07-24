bd2084269bcbc9ab7020f931556f8a0d5d69242b
Yegor  Abramenkov <yegorabramenkov@Yegors-MacBook-Pro.local>
2019-07-24 23:04:28 +0300

    test

--- CalculatorTests/RegularCalculatorTests.cs
+++ CalculatorTests/RegularCalculatorTests.cs
@@ -1,57 +1,89 @@
-using NUnit.Framework;
 using Calculator;
+using NUnit.Framework;
 
 namespace Tests
 {
-    public class Tests
+    [TestFixture(Description = "Test suite for regular calculator features")]
+    public class RegularCalculatorTests
     {
-        [TestFixture]
-        public class CalculatorTests
+        [SetUp] public void Initialize()
         {
+        }
 
-            // Calculator tests
-            [Test(Description = "Verifies the work of calculator ADDITION feature"), Order(1)]
-            public void TestAddition()
-            {
-                RegularCalculator calc = new RegularCalculator();
-                calc = new RegularCalculator();
-                Assert.AreEqual(3, calc.Addition(1, 2));
-            }
+        [TearDown] public void CleanUp()
+        {
+        }
 
-            [Test(Description = "Verifies the work of calculator SUBSTRACTION feature"), Order(2)]
-            public void TestSubstraction()
-            {
-                RegularCalculator calc = new RegularCalculator();
-                Assert.AreEqual(-1, calc.Subsctraction(1, 2));
-            }
-
-            //    public static TestCaseData[] TestCaseData = {
-            //    new TestCaseData(135),
-            //    new TestCaseData(2)
-            //};
-            [TestCase(135, 2, ExpectedResult = 67.5), Order(3)]
-            public double TestDivision(int a, int b)
-            {
-                RegularCalculator calc = new RegularCalculator();
-                return calc.Division(a, b);
-            }
-
-            [Test(Description = "Verifies the work of calculator MULTIPLICATION feature", ExpectedResult = 4), Order(4)]
-            public double TestMultiplication()
-            {
-                RegularCalculator calc = new RegularCalculator();
-                return calc.Multiplictation(2, 2);
-            }
+        public RegularCalculator calc = new RegularCalculator();
 
 
-            //Other tests
+        // Calculator tests
+        [TestCase(4, 2, 2), Description("test"), Order(1), MaxTime(1), Repeat(2), Retry(2)]
+        [TestCase(12, 6, 6)]
+        [TestCase(-2, 1, -3)]
+        [TestCase(102.5, 2.5, 100)]
+        public void TestAddition(double target, double variable1, double variable2)
+        {
+            Assert.AreEqual(target, calc.Addition(variable1, variable2));
+        }
 
-            [Test(Description = "Testing some funny string comparisions, this one fails on purpose"), Order(5)]
-            public void VerifyConsoleInput()
-            {
-                string testString = "This is the most clever test ever";
-                Assert.IsEmpty(testString);
-            }
+        [Test(Description = "Verifies the work of calculator SUBSTRACTION feature"), Order(4)]
+        public void TestSubstraction()
+        {
+            Assert.AreEqual(-1, calc.Subsctraction(1, 2));
+        }
+
+        [TestCase(135, 2, ExpectedResult = 67.5), Order(5)]
+        public double TestDivision(int a, int b)
+        {
+            return calc.Division(a, b);
+        }
+
+        [Test(Description = "Verifies the work of calculator MULTIPLICATION feature", ExpectedResult = 4), Order(6)]
+        public double TestMultiplication()
+        {
+            return calc.Multiplictation(2, 2);
+        }
+
+
+        //STRING TESTS
+
+        [Test(Description = "Testing some funny string comparisions, this one fails on purpose"), Order(7)]
+        [Ignore("For a reason")]
+        public void VerifyStringIsEmpty()
+        {
+            string testString = "This is the most clever test ever";
+            Assert.IsEmpty(testString);
+        }
+
+        [Test(Description = "Check if one string contains the other"), Order(8)]
+        public void CheckIfStringsContainEachOther()
+        {
+            string a = "This is a fun test";
+            string b = "test";
+            Assert.That(a.Contains(b));
+        }
+
+        [Test(Description = "Check if strings start with a certain letter"), Order(9)]
+        public void CheckIfStringStartsWith()
+        {
+            string a = "This is a fun test";
+            string b = "test";
+            Assert.That(a.ToLower().StartsWith("t") && b.ToLower().StartsWith("t"));
+        }
+
+        //ALWAYS PASS
+        [Test(Description = "This one is meant to pass"), Order(11)]
+        public void PassItAll()
+        {
+            Assert.Pass();
+        }
+
+        //ALWAYS FAIL
+        [Test(Description = "This one is meant to fail"), Order(12)]
+        public void FailItAll()
+        {
+            Assert.Fail();
         }
     }
 }
--- CalculatorTests/ScientificCalculatorTests.cs
+++ CalculatorTests/ScientificCalculatorTests.cs
@@ -3,46 +3,46 @@ using NUnit.Framework;
 using Calculator;
 namespace Tests
 {
-    [TestFixture]
+    [TestFixture (Description = "Test suite for scientific calculator features")]
     public class ScientificCalculatorTests
     {
+        ScientificCalculator calc = new ScientificCalculator();
 
-        [Test(Description = "MathPOW method verification"), Order(1)]
-        public void VerifyPow()
+
+        [Test(ExpectedResult = 4, Description = "MathPOW method verification"), Order(1)]
+        public double VerifyPow()
         {
-            ScientificCalculator calc = new ScientificCalculator();
-            Assert.AreEqual(4, calc.MathPowFunc(2, 2));
+            return calc.MathPowFunc(2, 2);
         }
 
-        [Test(Description = "GetPerscent method verification"), Order(2)]
-        public void VerifyGetPercent()
+        [Test(ExpectedResult = 5, Description = "GetPerscent method verification"), Order(2)]
+        public double VerifyGetPercent()
         {
-            ScientificCalculator calc = new ScientificCalculator();
-            Assert.AreEqual(5, calc.GetPercent(5, 100));
+            return calc.GetPercent(5, 100);
         }
 
         [Test(Description = ""), Order(3)]
         public void VerifyGetArraySum()
         {
             double[] doubleArray = { 1, 2, 3 };
-            ScientificCalculator calc = new ScientificCalculator();
-            Assert.AreEqual(6, calc.GetArraySum(doubleArray));
+            //Assert.AreEqual(6, calc.GetArraySum(doubleArray));
+            Assert.That(6, Is.EqualTo(calc.GetArraySum(doubleArray)), message: "Test output did not match an expected result.");
         }
 
         [Test(Description = "GetMaxArrayValue method verification"), Order(4)]
         public void VerifyGetMaxArrayValue()
         {
             double[] doubleArray = { 2, 2, 3, 5, 18, 0, 33, 1.6, 33.3 };
-            ScientificCalculator calc = new ScientificCalculator();
-            Assert.AreEqual(33.3, calc.GetMaxArrayValue(doubleArray));
+            //Assert.AreEqual(33.3, calc.GetArraySum(doubleArray));
+            Assert.That(33.3, Is.EqualTo(calc.GetMaxArrayValue(doubleArray)));
         }
 
         [Test(Description = "GetMaxArrayValue method verification"), Order(5)]
         public void VerifyGetMinArrayValue()
         {
             double[] doubleArray = { 2, 2, 3, 5, 18, 0, 33, 1.6, 33.3 };
-            ScientificCalculator calc = new ScientificCalculator();
-            Assert.AreEqual(33.3, calc.GetMinArrayValue(doubleArray));
+            //Assert.AreEqual(0, calc.GetMinArrayValue(doubleArray));
+            Assert.That(0, Is.LessThanOrEqualTo(calc.GetMinArrayValue(doubleArray)));
         }
     }
 }
